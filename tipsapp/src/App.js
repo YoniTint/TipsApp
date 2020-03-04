4e54cff53fba521d768d49dc8d9a011781bfe591
Yonatan Tin <yonitint1@gmail.com>
2020-03-04 15:49:42 +0200

    added table for the first time..

--- tipsapp/src/App.js
+++ tipsapp/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
+import MaterialTable from 'material-table';
 
 const particlesParams = {
 	    particles: {
@@ -21,20 +22,46 @@ const particlesParams = {
 	    },
 	  }
 
+
 class App extends Component {
     
 
+
+
 	render() {
+		const data = [
+			{
+				name: 'Yonatan',
+				surname: 'Tintpulver'
+			},
+
+			];
+
   		return (
-    		<div className="App">
+    		<div className="">
     			<Particles 
+    			    className='particles'
     				param={particlesParams}
-    				style={
-    					{
-    						width: '100%',	
-    					}
-    				}
-    			/>		
+    			/>
+    			<div className='table'>
+	    			<MaterialTable 
+	    			    columns={[
+	    			    	{ title: 'Name', field: 'name' },
+	    			    	{title: 'Surname', field: 'surname'},		
+	    			    ]}
+	    			    options={{
+	    			    	search: false,
+	    			    }}
+	    			    data={data}
+	            		actions={[  /* !!! need to check if there is onRowChange and if not, write it !!! */ 
+	              		  {
+	              		  	icon: 'd',
+	                		tooltip: 'Delete User',
+	                		//onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
+	              	      }
+	                    ]}
+	                />	
+                </div>		
    			</div>
   		);
 	}
