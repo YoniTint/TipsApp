8846381f49db1784e566c63dbb16845c35e506a1
Yonatan Tin <yonitint1@gmail.com>
2020-03-04 13:26:56 +0200

    background

--- tipsapp/src/App.js
+++ tipsapp/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesParams = {
+	    particles: {
+	        number: {
+	            value: 50,
+	        },
+	        size: {
+	            value: 3,
+	        },
+	    },
+	    interactivity: {
+	        events: {
+	            onhover: {
+	                enable: true,
+	                mode: "repulse",
+	            },
+	        },
+	    },
+	  }
+
+class App extends Component {
+    
+
+	render() {
+  		return (
+    		<div className="App">
+    			<Particles 
+    				param={particlesParams}
+    				style={
+    					{
+    						width: '100%',	
+    					}
+    				}
+    			/>		
+   			</div>
+  		);
+	}
+
 }
-
 export default App;
