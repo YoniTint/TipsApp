c1e95f17f9a911369b079520b63c9f681381fd89
Yonatan Tin <yonitint1@gmail.com>
2020-03-06 03:43:01 +0200

    making an attempt to use hooks and Table as an independent

--- tipsapp/src/App.js
+++ tipsapp/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
-import MaterialTable from 'material-table';
+import MaterialTableDemo from './components/Table/Table';
 
 const particlesParams = {
 	    particles: {
@@ -29,13 +29,7 @@ class App extends Component {
 
 
 	render() {
-		const data = [
-			{
-				name: 'Yonatan',
-				surname: 'Tintpulver'
-			},
 
-			];
 
   		return (
     		<div className="">
@@ -43,25 +37,7 @@ class App extends Component {
     			    className='particles'
     				param={particlesParams}
     			/>
-    			<div className='table'>
-	    			<MaterialTable 
-	    			    columns={[
-	    			    	{ title: 'Name', field: 'name' },
-	    			    	{title: 'Surname', field: 'surname'},		
-	    			    ]}
-	    			    options={{
-	    			    	search: false,
-	    			    }}
-	    			    data={data}
-	            		actions={[  /* !!! need to check if there is onRowChange and if not, write it !!! */ 
-	              		  {
-	              		  	icon: 'd',
-	                		tooltip: 'Delete User',
-	                		//onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
-	              	      }
-	                    ]}
-	                />	
-                </div>		
+    			<MaterialTableDemo />
    			</div>
   		);
 	}
